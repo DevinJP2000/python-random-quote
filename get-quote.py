b585a532648c9365599b54229b17235fab8ea27d
DevinJP2000 <devinjp2000@gmail.com>
2022-04-28 16:36:29 -0600

    Added user-defined amounts of quotes to display.

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,25 @@
 import random
 def primary():
+  correct = False
+  numquote = 1
+  while(correct == False):
+    print("How many quotes do you want?")
+    try:
+      numquote = int(input())
+      correct = True
+    except:
+      print("Sorry, that's not a number.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
+  while(numquote != 0):
+    last = 13
+    rnd = random.randint(0, last)
 
-  print(quotes[rnd])
+    print(quotes[rnd])
+    numquote = numquote - 1
 
 if __name__== "__main__":
   primary()
